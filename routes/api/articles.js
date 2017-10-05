dac58dc2525aed5423deffde50f81b6d03c41edb
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-04 22:24:20 -0700

    List all articles with limits and offsets to create pagination

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -71,6 +71,91 @@ router.delete('/:article', auth.required, function(req,res,next) {
 });
 
 
+// Get list of all articles with pages
+router.get('/', auth.optional, function(req,res,next) {
+    var query = {};
+    var limit = 20;
+    var offset = 0;
+    if (typeof req.query.limit !== 'undefined') {
+        limit = req.query.limit;
+    }
+    if(typeof req.query.offset !== 'undefined') {
+        offset = req.query.offset;
+    }
+    if (typeof req.query.tag !== 'undefined') {
+        query.tagList = {"$in": [req.query.tag]};
+    }
+
+    Promise.all([
+        req.query.author ? User.findOne({username: req.query.author}) : null,
+        req.query.favorited ? User.findOne({username: req.query.favorited}) : null
+    ]).then(function(results) {
+        var author = results[0];
+        var favoriter = results[1];
+        if (author) {
+            query.author = author._id;
+        }
+        if (favoriter) {
+            query._id = {$in: favoriter.favorites};
+        } else if (req.query.favorited) {
+            query._id = {$in: []};
+        }
+
+        return Promise.all([
+            Article.find(query)
+                .limit(Number(limit))
+                .skip(Number(offset))
+                .sort({createdAt: 'desc'})
+                .populate('author')
+                .exec(),
+            Article.count(query).exec(),
+            req.payload ? User.findById(req.payload.id) : null
+        ]).then(function(results) {
+            var articles = results[0];
+            var articleCount = results[1];
+            var user = results[2];
+            return res.json({
+                articles: articles.map(function(article) {
+                    return article.toJSONFor(user);
+                }),
+                articlesCount: articleCount
+            });
+        });
+    }).catch(next);
+});
+
+router.get('/feed', auth.required, function(req,res,next) {
+    var limit = 20;
+    var offset = 0;
+    if (typeof req.query.limit !== 'undefined') {
+        limit = req.query.limit;
+    }
+    if (typeof req.query.offset !== 'undefined') {
+        offset = req.query.offset;
+    }
+    User.findById(req.payload.id).then(function(user) {
+        if (!user) {return res.sendStatus(401);}
+        Promise.all([
+            Article.find({author: {$in: user.following}})
+                .limit(Number(limit))
+                .skip(Nmber(offset))
+                .populate('author')
+                .exec(),
+            Article.count({author: {$in: user.following}})
+        ]).then(function(results) {
+            var articles = results[0];
+            var articlesCount = results[1];
+            return res.json({
+                articles: articles.map(function(article) {
+                    return article.toJSONFor(user);
+                }),
+                articlesCount: articlesCount
+            });
+        }).catch(next);
+    });
+});
+
+
 // Favoriting mechanisms for article
 router.post('/:article/favorite', auth.required, function(req,res,next) {
     var articleId = req.article._id;
